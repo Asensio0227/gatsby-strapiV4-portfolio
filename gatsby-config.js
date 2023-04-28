a81e46a09dcb4e8fcb2843ffc38d8f47dc89b7b7
Asensio0227 <mtheesikhosana@gmail.com>
2023-04-28 11:28:26 +0200

    seo and strapiV4 commit

---
+++ gatsby-config.js
@@ -0,0 +1,48 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const strapiConfig = {
+  apiURL: process.env.STRAPI_API_URL,
+  accessToken: process.env.STRAPI_TOKEN,
+  collectionTypes: ["job", "project"],
+  singleTypes: ["about"],
+}
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `WebDev Portfolio`,
+    description: `Awesome WebDev Portfolio built with Gatsby and Strapi`,
+    titleTemplate: `%s | WebDev Portfolio`,
+    siteUrl: `https://gatsby-strapi-portfolio-backend.netlify.app`,
+    twitterUsername: `@Brendon52262477`,
+    image: `/mainImg.png`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `images`,
+        // Path to the directory
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-strapi`,
+      options: strapiConfig,
+    },
+  ],
+}
---
+++ src/components/Jobs.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react"
+import Title from "./Title"
+import { FaAngleDoubleRight } from "react-icons/fa"
+import { graphql, useStaticQuery } from "gatsby"
+import { Link } from "gatsby"
+
+const query = graphql`
+  query {
+    allStrapiJob(sort: { createdAt: DESC }) {
+      nodes {
+        position
+        company
+        date
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
+const Jobs = () => {
+  const data = useStaticQuery(query)
+  const {
+    allStrapiJob: { nodes: jobs },
+  } = data
+  const [value, setValue] = useState(0)
+  const { company, position, date, desc } = jobs[value]
+
+  return (
+    <section className="section jobs">
+      <Title title="experience" />
+      <div className="jobs-center">
+        {/* btn container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={index}
+                className={index === value ? "job-btn active-btn" : "job-btn"}
+                onClick={() => setValue(index)}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        {/* job info */}
+        <article className="job-info">
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date}</p>
+          {desc.map(item => {
+            return (
+              <div className="job-desc" key={item.id}>
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{item.name}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        more info
+      </Link>
+    </section>
+  )
+}
+
+export default Jobs
