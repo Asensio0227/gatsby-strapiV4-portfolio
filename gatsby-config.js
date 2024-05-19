6ae26f198b21f6b9b02df8b87b0f2cb2dc1ed401
Asensio0227 <mtheesikhosana@gmail.com>
2024-05-19 19:38:36 +0200

    env commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,13 +43,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-strapi`,
-      options: {
-        apiURL: `http://localhost:1337`,
-        accessToken: `8395e7ab9f29bba79e42f4cee746f6acbbe004448623730080edf1d3ddb1c40405f96a91cea7afefad7c5a922f11ed6067b3b0300b088668d20ae7c2326c19a73e7b264e60a99ef3625905e097821340b16914354e81e2ada3172b3e3636e897c563972c12b36fd9609cb544c5e93f5e3223609e886f91a4907f952f2c879827`,
-        queryLimit: 1000,
-        collectionTypes: ["job", "project"],
-        singleTypes: ["about"],
-      },
+      options: strapiConfig,
     },
   ],
 }
